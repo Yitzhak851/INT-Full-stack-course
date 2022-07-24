4dd6f8d7b0051003f803e36a267423f603e8107f
Yitzhak Bar Or <yitzhak@bynet.co.il>
2022-07-24 17:50:41 +0300

    Adding more some few folders

---
+++ 17. Lesson17(24-7-22)/script.js
@@ -0,0 +1,25 @@
+let divContainer;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, -1];
+let emptyCell = 8;
+let divCell;//TODO- finish
+
+
+function rand(min, max){
+    return min + Math.floor(Math.random() * (max - min))
+}
+
+
+function blabla{
+    
+}
+
+
+
+function blabla{
+    
+}
+
+
+function blabla{
+    
+}
