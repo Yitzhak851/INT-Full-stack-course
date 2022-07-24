3623cf2d5a887a79bda700bb50b7feeb532b570f
Yitzhak Bar Or <yitzhak@bynet.co.il>
2022-07-24 18:07:59 +0300

    small change

--- 17. Lesson17(24-7-22)/script.js
+++ 17. Lesson17(24-7-22)/script.js
@@ -2,24 +2,37 @@ let divContainer;
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, -1];
 let emptyCell = 8;
 let divCell;//TODO- finish
+let row;
+let col;
 
 
-function rand(min, max){
-    return min + Math.floor(Math.random() * (max - min))
+function rand (min, max){
+    return min + Math.floor(Math.random() * (max - min));
 }
 
-
-function blabla{
-    
+function row (i){
+    return (i-i%3)/3;
 }
 
-
-
-function blabla{
-    
+function col (i){
+    return (i%3);
 }
 
+function getIndex (row, col){
+    return 3*row +col;
+}
 
-function blabla{
-    
+function shuffle (){
+    let r = row(emptyCell);
+    let c = col(emptyCell);
+    if(emptyCell ==4){
+        //We have fowr option, this is the middle
+
+        if(rand(0,2) == 0){
+            // Change row
+            r +=rand(0,2) == 0 ? 1 : -1;
+        }else{
+
+        }
+    }
 }
